f6bcbf984f5310c6937d8d9bdaf687e1660c888d
kiorky <kiorky@cryptelium.net>
2010-02-07 08:31:33 +0100

    up

--- h.py
+++ h.py
@@ -1,10 +1,10 @@
 import os
 import sys
 def h(options, buildout):
+    c = os.getcwd()
+    os.chdir(options['compile-directory'])
+    os.system("autoreconf -ifv")
     if sys.platform.startswith('cygwin'):
-        c = os.getcwd()
-        os.chdir(options['compile-directory'])
-        os.system("autoreconf -ifv")
         r = open('config.h.in').readlines()
         r.append('\n\n#define _POSIX_C_SOURCE 200112L\n\n')
         open('config.h.in', 'w').writelines(r)
